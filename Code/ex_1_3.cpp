6dea18d925fa50abef29e41f0e333177b45ed7a9
mingzailao <mingzailao@163.com>
2016-09-21 15:35:47 +0800

    second

---
+++ Code/ex_1_3.cpp
@@ -0,0 +1,12 @@
+/////////////////////////////////////////////////////////////////////////
+// Exercise 1.3: Write a program to print Hello, World on the standard //
+// output.                                                             //
+/////////////////////////////////////////////////////////////////////////
+
+
+#include<iostream>
+int main()
+{
+    std::cout<<"Hello world"<<std::endl;
+    return 0;
+}
